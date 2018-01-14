597123f915493c4fd7c2e90c22303947d3f00d19
y14545028 <begum-2018@hotmail.com>
2018-01-14 23:12:58 +0000

    initial commit

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,92 @@
+class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:edit, :update, :show, :like]
+    before_action :require_user, except: [:show, :index, :like]
+    before_action :require_user_like, only: [:like]
+    before_action :require_same_user, only: [:edit, :update]
+    before_action :admin_user, only: :destroy
+    
+    def index
+        @recipes = Recipe.paginate(page: params[:page], per_page: 4) 
+    end
+    
+    def show
+        
+    end
+    
+    def new
+        @recipe = Recipe.new
+    end
+    
+    def create
+        @recipe = Recipe.new(recipe_params)
+        @recipe.chef = current_user
+        
+        if @recipe.save
+            flash[:success] = "Tarifiniz başarıyla oluşturuldu!"
+            redirect_to recipes_path
+        else 
+            render :new
+        end
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        if @recipe.update(recipe_params)
+            flash[:success] = "Tarifiniz başarıyla güncellendi!"
+            redirect_to recipe_path(@recipe)
+        else
+            render :edit 
+        end
+    end
+    
+    def like
+        like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
+        if like.valid?
+            flash[:success] = "Seçiminiz başarılı oldu"
+            redirect_to :back
+        else
+            flash[:danger] = "Sadece bir tarifi beğenebilir veya beğenemezsiniz." 
+            redirect_to :back
+        end
+    end
+    
+    def destroy
+        Recipe.find(params[:id]).destroy
+        flash[:success] = "Tarif Silindi"
+        redirect_to recipes_path
+    end
+    
+    private
+    
+        def recipe_params
+            params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
+        end
+        
+        def set_recipe
+            @recipe = Recipe.find(params[:id])
+        end
+        
+        def require_same_user
+            if current_user != @recipe.chef and !current_user.admin?
+                flash[:danger] = "Sadece kendi tariflerini düzenleyebilirsin"
+                redirect_to recipes_path
+            end
+            
+        end
+        
+        def require_user_like
+            if !logged_in?
+              flash[:danger] = "Bu işlemi gerçekleştirmek için giriş yapmış olmalısınız"
+              redirect_to :back
+            end
+            
+        end
+        
+        def admin_user
+            redirect_to recipes_path unless current_user.admin?
+        end
+            
+end
